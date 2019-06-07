9446b2ab075201124868b892902441ebe5e51afe
Evgeniy Gavryushin <job.egavr@yandex.ru>
2019-06-07 22:47:14 +0300

    fix: generating of chunks should not be affected by the order of the tests

--- test/lib/chunks.js
+++ test/lib/chunks.js
@@ -12,7 +12,7 @@ describe('lib/chunks', () => {
         chunks.addTest(test1);
         chunks.addTest(test2);
 
-        assert.deepEqual(chunks.get(), [[test1], [test2]]);
+        assert.deepEqual(chunks.get(), [[test2], [test1]]);
     });
 
     it('should distribute odd number of tests on chunks', () => {
@@ -25,7 +25,7 @@ describe('lib/chunks', () => {
         chunks.addTest(test2);
         chunks.addTest(test3);
 
-        assert.deepEqual(chunks.get(), [[test1, test3], [test2]]);
+        assert.deepEqual(chunks.get(), [[test2, test1], [test3]]);
     });
 
     it('should not distribute tests with the same full title on different chunks', () => {
@@ -51,6 +51,28 @@ describe('lib/chunks', () => {
         chunks.addTest(test3);
         chunks.addTest(test4);
 
-        assert.deepEqual(chunks.get(), [[test3, test4], [test1, test2]]);
+        assert.deepEqual(chunks.get(), [[test3, test4], [test2, test1]]);
+    });
+
+    it.only('should not be affected by the order of the tests', () => {
+        const chunks1 = Chunks.create({count: 2});
+        const chunks2 = Chunks.create({count: 2});
+
+        const test1 = stubTest({title: 'foo', browserId: 'bro1'});
+        const test2 = stubTest({title: 'bar', browserId: 'bro2'});
+        const test3 = stubTest({title: 'baz', browserId: 'bro1'});
+        const test4 = stubTest({title: 'qux', browserId: 'bro2'});
+
+        chunks1.addTest(test1);
+        chunks1.addTest(test2);
+        chunks1.addTest(test3);
+        chunks1.addTest(test4);
+
+        chunks2.addTest(test4);
+        chunks2.addTest(test3);
+        chunks2.addTest(test2);
+        chunks2.addTest(test1);
+
+        assert.deepEqual(chunks1.get(), chunks2.get());
     });
 });
